ab44e34b0f586f4add9c7874fa08b2f211d8b822
MohamedMedhat <kingmahamed206@gmail.com>
2025-05-16 07:00:42 +0300

    create simple category view for test

--- ecom/urls.py
+++ ecom/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import *
+from category.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("cat/",show_categ,name="show_categ"),
 ]
