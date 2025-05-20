965e7b5eeb5d8ac6d7e83cad56789a1bb1946633
MohamedMedhat <kingmahamed206@gmail.com>
2025-05-20 14:06:16 +0300

    crud operation for catgaory using generic view

--- category/urls.py
+++ category/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
-
+from .views import (
+    CategoryListView,
+    CategoryCreateView,
+    CategoryUpdateView,
+    CategoryDeleteView,
+)
 app_name = 'category'
 
 urlpatterns = [
@@ -8,4 +13,12 @@ urlpatterns = [
     path('create/', views.category_create, name='create'),
     path('edit/<int:pk>/', views.category_edit, name='edit'),
     path('delete/<int:pk>/', views.category_delete, name='delete'),
+    
+    path('gv', CategoryListView.as_view(), name='category_list'),
+    path('createg/', CategoryCreateView.as_view(), name='create'),
+    path('editg/<int:pk>/', CategoryUpdateView.as_view(), name='edit'),
+    path('deleteg/<int:pk>/', CategoryDeleteView.as_view(), name='delete'),
 ]
+
+
+
--- category/views.py
+++ category/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Category
 from product.models import Product
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 def category_list(request):
     categories = Category.objects.all()
@@ -24,10 +26,33 @@ def category_edit(request, pk):
     return render(request, 'category/edit.html', {'category': category})
 
 
-
-
 def category_delete(request, pk):
     category = get_object_or_404(Category, pk=pk)
     if request.method == 'POST':
         category.delete()
         return redirect('category:category_list')
+
+
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category/list.html'
+    context_object_name = 'categories'
+
+class CategoryCreateView(CreateView):
+    model = Category
+    template_name = 'category/createg.html'
+    fields = ['name', 'description']
+    success_url = reverse_lazy('category:category_list')
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    template_name = 'category/editg.html'
+    fields = ['name', 'description']
+    success_url = reverse_lazy('category:category_list')
+
+class CategoryDeleteView(DeleteView):
+    model = Category
+    template_name = 'category/delete.html'
+    success_url = reverse_lazy('category:category_list')
